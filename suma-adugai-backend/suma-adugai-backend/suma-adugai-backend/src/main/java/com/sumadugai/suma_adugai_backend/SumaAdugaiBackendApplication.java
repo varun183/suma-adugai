ae2e40826d7f6856421ff809dbb7d4b160d4fbff
S Varun <varunshanker1718@gmail.com>
2024-12-31 14:36:07 +0530

    setup react and spring environment

---
+++ suma-adugai-backend/suma-adugai-backend/suma-adugai-backend/src/main/java/com/sumadugai/suma_adugai_backend/SumaAdugaiBackendApplication.java
@@ -0,0 +1,14 @@
+package com.sumadugai.suma_adugai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumaAdugaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumaAdugaiBackendApplication.class, args);
+		System.out.println("Hello spring");
+	}
+
+}
