f89e100eff0c6cbeae065560ea1ea83952db9496
S Varun <varunshanker1718@gmail.com>
2024-12-31 14:36:07 +0530

    setup react and spring environment

---
+++ suma-adugai-backend/suma-adugai-backend/suma-adugai-backend/src/test/java/com/sumadugai/suma_adugai_backend/SumaAdugaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumadugai.suma_adugai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumaAdugaiBackendApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
